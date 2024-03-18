33382e467e19e890aa31bfdaccd47ef42b15a32b
Lillian Nguyen <lillianxnguyen@gmail.com>
2024-03-17 21:36:16 -0500

    completed contact/social section

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// showing sidebar messages on hover
 const sidebarProduct = document.getElementById('sidebar-products')
 sidebarProduct.addEventListener('mouseover', showProducts)
 sidebarProduct.addEventListener('mouseout', hideProducts)
@@ -32,4 +33,42 @@ function showAbout() {
 
 function hideAbout() {
     document.querySelector('.ribbon-3').style.visibility = 'hidden'
+}
+
+//showing social media labels on hover 
+
+const instagram = document.getElementById('ig-logo')
+instagram.addEventListener('mouseover', showIgLabel)
+instagram.addEventListener('mouseout', hideIgLabel)
+
+function showIgLabel() {
+    document.querySelector('.ig-label').style.visibility = 'initial'
+}
+
+function hideIgLabel() {
+    document.querySelector('.ig-label').style.visibility = 'hidden'
+}
+
+const youtube = document.getElementById('youtube-logo')
+youtube.addEventListener('mouseover', showYoutubeLabel)
+youtube.addEventListener('mouseout', hideYoutubeLabel)
+
+function showYoutubeLabel() {
+    document.querySelector('.youtube-label').style.visibility = 'initial'
+}
+
+function hideYoutubeLabel() {
+    document.querySelector('.youtube-label').style.visibility = 'hidden'
+}
+
+const email = document.getElementById('paper-plane')
+email.addEventListener('mouseover', suggestEmail)
+email.addEventListener('mouseout', hideEmail)
+
+function suggestEmail() {
+    document.querySelector('.email-label').style.visibility = 'initial'
+}
+
+function hideEmail() {
+    document.querySelector('.email-label').style.visibility = 'hidden'
 }
